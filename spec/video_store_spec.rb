943c91b73ba47152eb366f51771472a8faf05b81
Ben <ben.dalton@madetech.com>
2020-10-15 17:28:06 +0100

    2nd test passes -statement builder with no rentals

--- spec/video_store_spec.rb
+++ spec/video_store_spec.rb
@@ -14,6 +14,35 @@ RSpec.describe VideoStore do
 
         end
 
+        it 'the statement contains no rentals' do
+            # Arrange
+            video_store = VideoStore.new(customer_name: "Dave Richards", movies: [])
+            expected_statement = <<~STATEMENT
+                Welcome Dave Richards
+                You have no rentals
+                You owe £0.0
+                You gained 0 frequent renter points
+            STATEMENT
 
+            # Act
+            statement = video_store.statement_builder
+
+            # Assert
+            expect(statement).to eq(expected_statement)
+        end
+    end
+
+    context 'when a customer has rented one movie for one day' do
+        xit 'records that movie on their statement' do
+            # Arrange
+            video_store = VideoStore.new(customer_name: "Dave Richards", movies: [])
+
+            # Act
+
+            # Assert
+
+        end
+    end
+end
 
 
