28df3ff2cd39b8430b44fbbe1bc44831d8056cf8
Ben <ben.dalton@madetech.com>
2020-10-16 16:29:01 +0100

    unit tests for dynamically printing movie name in statement builder

--- lib/video_store.rb
+++ lib/video_store.rb
@@ -7,11 +7,37 @@ class VideoStore
     end
 
     def statement_builder
-        <<~STATEMENT
-            Welcome #{@customer_name}
-            You have no rentals
-            You owe £0.0
-            You gained 0 frequent renter points
-        STATEMENT
+        if movies.empty?
+            <<~STATEMENT
+                Rental Record for #{customer_name}
+                You have no rentals
+                You owe £0.0
+                You gained 0 frequent renter points
+            STATEMENT
+
+        else
+            <<~STATEMENT
+                Rental Record for #{customer_name}
+                #{create_movie_line(movies)}
+                You owe £2.0
+                You gained 1 frequent renter points
+            STATEMENT
+        end
+    end
+
+    def create_movie_line(movies)
+        movie_line = ''
+        movies.each do |movie|
+            movie_line = movie + " £2.0"
+        end
+        movie_line
     end
 end
+
+# dynamically include cost of movie
+
+# MovieTitles = {
+#     RegularMovies => ["Transformers","Triple Frontier", "The Expert Coder"]
+#     NewReleases => [],
+#     ChildrenMovies => []
+# }
--- spec/video_store_spec.rb
+++ spec/video_store_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe VideoStore do
     context 'when no movies are rented' do
         it 'the statement contains the customer name' do
             # Arrange
-            video_store = VideoStore.new(customer_name: "Dave Richards")            
+            video_store = VideoStore.new(customer_name: "Dave Richards")
 
             # Act
             statement = video_store.statement_builder
@@ -18,7 +18,7 @@ RSpec.describe VideoStore do
             # Arrange
             video_store = VideoStore.new(customer_name: "Dave Richards", movies: [])
             expected_statement = <<~STATEMENT
-                Welcome Dave Richards
+                Rental Record for Dave Richards
                 You have no rentals
                 You owe £0.0
                 You gained 0 frequent renter points
@@ -33,14 +33,40 @@ RSpec.describe VideoStore do
     end
 
     context 'when a customer has rented one movie for one day' do
-        xit 'records that movie on their statement' do
+        it 'records the movie on their statement' do
             # Arrange
-            video_store = VideoStore.new(customer_name: "Dave Richards", movies: [])
+            video_store = VideoStore.new(customer_name: "Dave Richards", movies: ["Transporter"])
+
+            expected_statement = <<~STATEMENT
+                Rental Record for Dave Richards
+                Transporter £2.0
+                You owe £2.0
+                You gained 1 frequent renter points
+            STATEMENT
 
             # Act
+            statement = video_store.statement_builder
 
             # Assert
+            expect(statement).to eq(expected_statement)
+        end
 
+        it 'records a different movie on their statement' do
+            # Arrange
+            video_store = VideoStore.new(customer_name: "Dave Richards", movies: ["Triple Frontier"])
+
+            expected_statement = <<~STATEMENT
+                Rental Record for Dave Richards
+                Triple Frontier £2.0
+                You owe £2.0
+                You gained 1 frequent renter points
+            STATEMENT
+
+            # Act
+            statement = video_store.statement_builder
+
+            # Assert
+            expect(statement).to eq(expected_statement)
         end
     end
 end
