7089fa2a17c397c712cdc379638f3e9f839fc433
Ben <ben.dalton@madetech.com>
2020-10-16 16:25:31 +0100

    wrote second acceptance test, 1 movie for 2 days

--- spec/acceptance_tests/acceptance_tests_spec.rb
+++ spec/acceptance_tests/acceptance_tests_spec.rb
@@ -5,8 +5,28 @@ RSpec.describe 'given a statement is requested by Dave Richards' do
         it 'returns a statement with with no rentals, no fees and no frequent renter points' do
             # Arrange
             video_store = VideoStore.new(customer_name: "Dave Richards", movies: [])
-            expected_statement = <<~STATEMENT 
-                Welcome Dave Richards
+            expected_statement = <<~STATEMENT
+                Rental Record for Dave Richards
+                You have no rentals
+                You owe £0.0
+                You gained 0 frequent renter points
+            STATEMENT
+
+
+            # Act
+            statement = video_store.statement_builder
+
+            # Assert
+            expect(statement).to eq(expected_statement)
+        end
+    end
+
+    context 'when there is currently 1 Regular movie rental for 2 days' do
+        xit 'returns a statement with with, the movie, fees and frequent renter points' do
+            # Arrange
+            video_store = VideoStore.new(customer_name: "Dave Richards", movies: [])
+            expected_statement = <<~STATEMENT
+                Rental Record for Dave Richards
                 You have no rentals
                 You owe £0.0
                 You gained 0 frequent renter points
